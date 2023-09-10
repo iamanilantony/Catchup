32d2dcd02bd03357a4c4133b38dd82fef61c04fb
Anil Antony <anilantony3134@gmail.com>
2023-09-10 11:06:59 +0530

    feat: Add contact modal added

--- app/modals/add-contact-modal.tsx
+++ app/modals/add-contact-modal.tsx
@@ -1,8 +1,17 @@
 import Modal from "@/components/shared/modal";
+import { Button } from "@/components/ui/button";
 import { ContactCreationRequest } from "@/lib/validators/contact";
 import axios from "axios";
 import Link from "next/link";
-import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { useRouter } from "next/router";
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  useState
+} from "react";
 import { useMutation } from "react-query";
 
 function AddContactModalHelper({
@@ -19,6 +28,8 @@ function AddContactModalHelper({
     relation: ""
   });
 
+  const router = useRouter();
+
   const { name, duration, relation } = data;
 
   return (
@@ -32,7 +43,7 @@ function AddContactModalHelper({
           href="/contacts"
           className="text-center text-xs text-gray-500 underline underline-offset-4 hover:text-gray-400"
         >
-          Show Your current contacts
+          Show Your current contacts1
         </Link>
         <form
           onSubmit={async (e: FormEvent<HTMLFormElement>) => {
@@ -55,11 +66,113 @@ function AddContactModalHelper({
                   payload
                 );
                 return data;
+              },
+              onError: () => {
+                return;
+              },
+              onSuccess: () => {
+                router.push("");
+                //toast
               }
             });
           }}
-        ></form>
+          className="flex flex-col space-y-6 bg-gray-50 px-4 py-8 text-left sm:px-16"
+        >
+          <div>
+            <label htmlFor="name" className="items flex">
+              <p className="block text-sm font-medium text-gray-700">
+                Contact Name
+              </p>
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+              <input
+                name="name"
+                id="name"
+                type="text"
+                required
+                autoFocus
+                autoComplete="off"
+                className="block w-full rounded-md border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => {
+                  setData({ ...data, name: e.target.value });
+                }}
+                aria-invalid="true"
+              />
+            </div>
+          </div>
+          <label htmlFor="duration" className="items flex">
+            <p className="block text-sm font-medium text-gray-700">
+              Duration to contact (in days)
+            </p>
+          </label>
+          <div className="mt-1 flex rounded-md shadow-sm">
+            <input
+              name="duration"
+              id="duration"
+              type="number"
+              // min='2' max = '39'
+              required
+              autoComplete="off"
+              className="block w-full border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+              placeholder="How frequently do you want to contact"
+              value={duration}
+              onChange={(e) => {
+                setData({
+                  ...data,
+                  duration: e.currentTarget.value as unknown as number
+                });
+              }}
+              aria-invalid="true"
+            />
+          </div>
+          <label htmlFor="duration" className="items flex">
+            <p className="block text-sm font-medium text-gray-700">
+              Relationship
+            </p>
+          </label>
+          <div className="mt-1 flex rounded-md shadow-sm">
+            <input
+              name="relation"
+              id="relation"
+              type="number"
+              // min='2' max = '39'
+              required
+              autoComplete="off"
+              className="block w-full border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+              placeholder="How frequently do you want to contact"
+              value={relation}
+              onChange={(e) => {
+                setData({
+                  ...data,
+                  relation: e.currentTarget.value
+                });
+              }}
+              aria-invalid="true"
+            />
+          </div>
+          <Button disabled={saving} title="create project" />
+        </form>
       </div>
     </Modal>
+  );
+}
+
+export function useAddContactModal() {
+  const [showAddContactModal, setShowAddContactModal] = useState(false);
+
+  const AddContactModal = useCallback(() => {
+    return (
+      <AddContactModalHelper
+        showAddContactModal={showAddContactModal}
+        setShowAddContactModal={setShowAddContactModal}
+      />
+    );
+  }, [showAddContactModal, setShowAddContactModal]);
+
+  return useMemo(
+    () => ({ setShowAddContactModal, AddContactModal }),
+    [setShowAddContactModal, AddContactModal]
   );
 }
