977aadb126c58948ec6bfcddbfa7d744ea546f79
Anil Antony <anilantony3134@gmail.com>
2023-09-18 06:19:19 +0530

    feat: date validator

--- lib/info/DateObj.ts
+++ lib/info/DateObj.ts
@@ -7,7 +7,7 @@ const daysOfWeek = [
     "Fri",
     "Sat"
   ];
-const dateObj = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+const dateObj = [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     .map((e) => {
       const currentDate = new Date();
       currentDate.setDate(currentDate.getDate() - e);
