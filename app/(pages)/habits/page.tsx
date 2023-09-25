d0543517f6119cfc79d1454f560fc0f6de5efc3f
Anil Antony <anilantony3134@gmail.com>
2023-09-25 06:59:52 +0530

    feat: Added Habit page content

--- app/(pages)/habits/page.tsx
+++ app/(pages)/habits/page.tsx
@@ -1,18 +1,31 @@
-import HabitCard from "@/components/home/habits";
 import dateObj from "@/lib/info/DateObj";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
 
 export default function page() {
+  const habits = ["", "nofap", "read", "workout", "code", "sleep", "meditate"];
   return (
     <>
       <div className="z-10 ml-40 w-full max-w-7xl px-5 xl:px-0">
         <div className="flex">
-          {dateObj.map((day) => (
-            <div>
-              {day.date}
-              {day.day}
+          <div className="mx-6 my-2 mt-20 flex cursor-pointer flex-col text-center">
+            {habits.map((habit) => (
+              <p className="my-[12px] font-semibold">{habit}</p>
+            ))}
+          </div>
+          {dateObj.map((current) => (
+            <div className="mx-6 my-2 flex cursor-pointer flex-col justify-center text-center">
+              <p>{current.day}</p>
+              <p>{current.date}</p>
+              <BsFillCheckCircleFill className="ml-1 mt-8 text-green-600" />
+
+              <ImCross className="ml-1 mt-8 text-red-600" />
+              <BsFillCheckCircleFill className="ml-1 mt-8 text-green-600" />
+              <BsFillCheckCircleFill className="ml-1 mt-8 text-green-600" />
+              <BsFillCheckCircleFill className="ml-1 mt-8 text-green-600" />
+              <BsFillCheckCircleFill className="ml-1 mt-8 text-green-600" />
             </div>
           ))}
-          <HabitCard />
         </div>
       </div>
     </>
--- lib/info/DateObj.ts
+++ lib/info/DateObj.ts
@@ -7,13 +7,14 @@ const daysOfWeek = [
     "Fri",
     "Sat"
   ];
-const dateObj = [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const dateObj = [ -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     .map((e) => {
       const currentDate = new Date();
       currentDate.setDate(currentDate.getDate() - e);
+      const date = currentDate.getDate() < 10 ? currentDate.getDate().toString().padStart(2,'0') : currentDate.getDate().toString() 
       return {
         day: daysOfWeek[currentDate.getDay()],
-        date: currentDate.getDate().toString()
+        date: date
       };
     })
     .reverse();
