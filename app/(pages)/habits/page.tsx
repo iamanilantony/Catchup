99c7c27b7f14142a044b0a20ffbb3a9e806af7af
Anil Antony <anilantony3134@gmail.com>
2023-09-25 10:30:25 +0530

    feat: Added habit panel

--- app/(pages)/habits/page.tsx
+++ app/(pages)/habits/page.tsx
@@ -1,8 +1,75 @@
+"use client";
+
 import dateObj from "@/lib/info/DateObj";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import { ImCross } from "react-icons/im";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable
+} from "@tanstack/react-table";
+import { useState } from "react";
 
 export default function page() {
+  type Habit = {
+    name: string;
+    completion: number;
+    streak: number;
+    attempts: number;
+    notes: string;
+  };
+
+  const defaultData: Habit[] = [
+    {
+      name: "PMO",
+      completion: 2,
+      streak: 2,
+      attempts: 2,
+      notes: "string"
+    },
+    {
+      name: "Read",
+      completion: 2,
+      streak: 2,
+      attempts: 2,
+      notes: "string"
+    },
+    {
+      name: "Study",
+      completion: 2,
+      streak: 2,
+      attempts: 2,
+      notes: "string"
+    }
+  ];
+
+  const columnHelper = createColumnHelper<Habit>();
+
+  const columns = [
+    columnHelper.accessor("name", {
+      cell: (info) => info.getValue()
+    }),
+    columnHelper.accessor("completion", {
+      cell: (info) => info.getValue()
+    }),
+    columnHelper.accessor("attempts", {
+      cell: (info) => info.getValue()
+    }),
+    columnHelper.accessor("notes", {
+      cell: (info) => info.getValue()
+    }),
+    columnHelper.accessor("streak", {
+      cell: (info) => info.getValue()
+    })
+  ];
+  const [data, setData] = useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  });
   const habits = ["", "nofap", "read", "workout", "code", "sleep", "meditate"];
   return (
     <>
@@ -27,6 +94,51 @@ export default function page() {
             </div>
           ))}
         </div>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.footer,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        </table>
       </div>
     </>
   );
