0a1406e0e9cf0b6e6230a7b0bd00004c306a26a5
Anil Antony <anilantony3134@gmail.com>
2023-10-28 10:22:17 +0530

    feat: Added expense changes

--- app/(pages)/expensetracker/page.tsx
+++ app/(pages)/expensetracker/page.tsx
@@ -1,27 +1,46 @@
+import DailySummaryCard from "@/components/expensetracker/dailySummaryCard";
+import SingleExpenseCard from "@/components/expensetracker/singleExpenseCard";
 import HabitCard from "@/components/home/habits";
 
 export default function page() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  const dateObj = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5].map((e) => {
-    return {
-      day: new Date(new Date().getDay() - e),
-      date: new Date(new Date().getDate() - e)
-    };
-  });
-  console.log(dateObj);
   return (
     <>
       <div className="z-10 ml-40 w-full max-w-7xl px-5 xl:px-0">
-        <div className="flex">
-          <HabitCard />
+        <div className="flex flex-col">
+          <div className="rounded bg-gray-100 px-2 pb-2">
+            <h1 className="my-2 text-center text-2xl font-bold">Oct 2023</h1>
+            <div className="flex justify-between px-2">
+              <p>Daily</p>
+              <p>Calender</p>
+              <p>Monthly</p>
+              <p>Summary</p>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <div>
+              <p>Income</p>
+              <p className="text-blue-400">₹ 76200</p>
+            </div>
+            <div>
+              <p>Exp</p>
+              <p className="text-red-400">₹ 76200</p>
+            </div>
+            <div>
+              <p>Total</p>
+              <p>₹ 76200</p>
+            </div>
+          </div>
+          <hr />
+          <div className="bg-transparent">
+            <DailySummaryCard />
+            <DailySummaryCard />
+            <DailySummaryCard />
+            <DailySummaryCard />
+            <DailySummaryCard />
+            <DailySummaryCard />
+          </div>
         </div>
       </div>
     </>
